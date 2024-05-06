b4857b826db05c3919cc34d87e6926a90db56193
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2024-05-06 15:54:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[1001];
+
+    arr[0] = 1;
+    arr[1] = 2;
+
+    for(int i=2; i<n; i++) {
+        arr[i] = (arr[i-1] + arr[i-2])%10007;
+    }
+
+    cout << arr[n-1];
+
+    return 0;
+}
