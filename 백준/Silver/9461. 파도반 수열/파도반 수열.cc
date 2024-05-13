e46b936a3e96175999de6189d63a829dad3aaffe
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2024-05-14 01:11:52 +0900

    [Silver III] Title: 파도반 수열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+long long int wave(int n);
+int main() {
+    int t;
+    cin >> t;
+
+    for (int i=0; i<t; i++) {
+        int n;
+        cin >> n;
+
+        cout << wave(n) << '\n';
+    }
+
+    return 0;
+}
+
+long long int wave(int n) {
+    long long int arr[102];
+
+    arr[1] = 1;
+    arr[2] = 1;
+    arr[3] = 1;
+    arr[4] = 2;
+    arr[5] = 2;
+
+    for(int i=6; i<=n; i++) {
+        arr[i] = arr[i-1] + arr[i-5];
+    }
+
+    return arr[n];
+}
