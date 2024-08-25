5b7b40ff1cff5f6ff6201fcd8608a266a4a8ed5b
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2024-08-25 16:18:59 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 120 ms, Memory: 16448 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    unordered_map<string, string> memo;
+
+    int n, m;
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++) {
+        string site;
+        string password;
+        cin >> site >> password;
+
+        memo[site] = password;
+       // memo.insert({site, password});
+    }
+
+    for(int i=0; i<m; i++) {
+        string siteToFind;
+        cin >> siteToFind;
+
+        // auto result = memo.find(siteToFind);
+        // cout << result->second << '\n';
+
+        cout << memo[siteToFind] << '\n';
+    }
+
+    return 0;
+}
