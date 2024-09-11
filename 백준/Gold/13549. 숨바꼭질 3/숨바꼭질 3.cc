e4d5fb39959073013d5c902ddcf8225c4b7179b3
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2024-09-11 21:04:39 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2420 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, k;
+  cin >> n >> k;
+
+  queue<int> q;
+  int visited[100001] = {0};
+
+  q.push(n);
+  visited[n] = 1;
+
+  while (visited[k] == 0) {
+    int tmp = q.front();
+    q.pop();
+
+    if (tmp * 2 <= 100000 && visited[tmp * 2] == 0) {
+      q.push(tmp * 2);
+      visited[tmp * 2] = visited[tmp];
+    }
+
+    if (tmp - 1 >= 0 && visited[tmp - 1] == 0) {
+      q.push(tmp - 1);
+      visited[tmp - 1] = visited[tmp] + 1;
+    }
+
+    if (tmp + 1 <= 100000 && visited[tmp + 1] == 0) {
+      q.push(tmp + 1);
+      visited[tmp + 1] = visited[tmp] + 1;
+    }
+  }
+
+  cout << visited[k] - 1;
+  return 0;
+}
