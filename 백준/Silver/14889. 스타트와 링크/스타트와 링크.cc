98575996062d421e92e18d812388d083ad55ad87
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2024-11-16 18:26:48 +0900

    [Silver I] Title: 스타트와 링크, Time: 80 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,7 +1,9 @@
-// 14889
 #include <algorithm>
 #include <iostream>
+#include <vector>
+
 using namespace std;
+using vii = vector<int>;
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -9,51 +11,58 @@ int main() {
 
   int n;
   cin >> n;
-  vector<vector<int> > capacity(n + 1, vector<int>(n + 1, 0));
 
+  vector<vii> capacity(n, vii(n, 0));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       cin >> capacity[i][j];
     }
   }
 
-  vector<int> people(n, 1);
+  vector<int> person;
   for (int i = 0; i < n / 2; i++) {
-    people[i] = 0;
+    person.push_back(0);
+    person.push_back(1);
   }
 
-  int minGap = 987654321;
+  int min = 999999;
   do {
-    int startScore = 0, linkScore = 0;
-    vector<int> startTeam;
-    vector<int> linkTeam;
+    vector<int> start;
+    vector<int> link;
 
     for (int i = 0; i < n; i++) {
-      if (people[i] == 1) {
-        startTeam.push_back(i);
-      } else {
-        linkTeam.push_back(i);
-      }
+      if (person[i] == 0)
+        start.push_back(i);
+      else
+        link.push_back(i);
     }
 
+    int start_capacity = 0;
     for (int i = 0; i < n / 2; i++) {
-      for (int j = i + 1; j < n / 2; j++) {
-        startScore += capacity[startTeam[i]][startTeam[j]];
-        startScore += capacity[startTeam[j]][startTeam[i]];
-
-        linkScore += capacity[linkTeam[i]][linkTeam[j]];
-        linkScore += capacity[linkTeam[j]][linkTeam[i]];
+      for (int j = 0; j < n / 2; j++) {
+        if (i < j) {
+          start_capacity += capacity[start[i]][start[j]];
+          start_capacity += capacity[start[j]][start[i]];
+        }
       }
     }
 
-    int gap = startScore > linkScore ? startScore - linkScore
-                                     : linkScore - startScore;
+    int link_capacity = 0;
+    for (int i = 0; i < n / 2; i++) {
+      for (int j = 0; j < n / 2; j++) {
+        if (i < j) {
+          link_capacity += capacity[link[i]][link[j]];
+          link_capacity += capacity[link[j]][link[i]];
+        }
+      }
+    }
 
-    if (minGap > gap)
-      minGap = gap;
-  } while (next_permutation(people.begin(), people.end()));
+    if (min > abs(start_capacity - link_capacity))
+      min = abs(start_capacity - link_capacity);
 
-  cout << minGap;
+  } while (next_permutation(person.begin(), person.end()));
+
+  cout << min;
 
   return 0;
 }
