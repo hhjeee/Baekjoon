4767e781ef39ef959173a8ed86540f8d504924b0
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-02-06 02:44:46 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  int x, y;
+  cin >> x >> y;
+
+  long long int prev_z = ((long long)y * 100) / x; // y / x * 100
+
+  if (prev_z >= 99) {
+    cout << -1;
+    return 0;
+  }
+
+  int left = 1, right = 2000000000;
+  int answer = -1;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    long long int new_z = ((long long)(y + mid) * 100) / (x + mid);
+
+    if (new_z > prev_z) {
+      answer = mid;
+      right = mid - 1;
+    } else
+      left = mid + 1;
+  }
+
+  cout << answer;
+
+  return 0;
+}
