bb96efc9e65a1b7b34cdc9a6a28a2ceffe3806c9
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-04-15 16:15:13 +0900

    [level 4] Title: 단어 퍼즐, Time: 9.95 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.cpp
+++ 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.cpp
@@ -5,37 +5,32 @@
 #include <climits>
 using namespace std;
 
-bool compare(string a, string b){
-    return a.length() > b.length();
-}
 bool isSame(const string& t, const string& str, int i){
     int len = str.length();
-    int start = i - len;
     for(int j=0; j<len; j++){
-        if(t[start + j] != str[j]) return false;
+        if(t[i-len+j] != str[j]) return false;
     }
     return true;
 }
+
 int solution(vector<string> strs, string t)
 {
     int n = t.length();
     vector<int> dp(n+1, INT_MAX);
     dp[0] = 0;
-    
-    sort(strs.begin(), strs.end(), compare);
-    
+
     for(int i=1; i<=n; i++){
-        for(string str:strs){
+        for(const string& str : strs){
             int len = str.length();
-            
-            if(len > i) continue; 
-            if(dp[i-len] == INT_MAX) continue; 
-            
+
+            if(len > i) continue;
+            if(dp[i-len] == INT_MAX) continue;
+
             if(isSame(t, str, i)){
                 dp[i] = min(dp[i], dp[i-len]+1);
             }
         }
     }
-    
+
    return (dp[n] == INT_MAX) ? -1 : dp[n];
 }
