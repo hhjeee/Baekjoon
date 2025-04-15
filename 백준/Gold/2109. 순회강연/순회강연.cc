cfe5b7a69e9cc55a4ab0d5fab73be270fc2072aa
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-04-15 18:25:12 +0900

    [Gold III] Title: 순회강연, Time: 16 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,10 +1,35 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
-using pii = pair<int, int>;
 
-bool compare(pii a, pii b) { return a.first > b.first; }
+struct lecture {
+  int pay, day;
+};
+struct compare {
+  bool operator()(lecture a, lecture b) {
+    if (a.pay == b.pay)
+      return a.day > b.day;
+    return a.pay < b.pay;
+  }
+};
+
+vector<int> days;
+void find_day(int pay, int day) {
+  if (!days[day])
+    days[day] = pay;
+  else {
+    int tmp = day - 1;
+    while (tmp > 0) {
+      if (!days[tmp]) {
+        days[tmp] = pay;
+        break;
+      }
+      tmp--;
+    }
+  }
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -13,31 +38,25 @@ int main() {
   int n;
   cin >> n;
 
-  vector<pii> univ(n);
-  for (int i = 0; i < n; i++) {
+  priority_queue<lecture, vector<lecture>, compare> pq;
+  int max_day = 0;
+  while (n--) {
     int p, d;
     cin >> p >> d;
-    univ.push_back({p, d});
+    pq.push({p, d});
+    max_day = max(max_day, d);
   }
-  sort(univ.begin(), univ.end(), compare); // p 내림차순
+
+  days.resize(max_day+1, 0);
+
+  while (!pq.empty()) {
+    find_day(pq.top().pay, pq.top().day);
+    pq.pop();
+  }
 
   int sum = 0;
-  int check[10001] = {0};
-  for (int i = 0; i < n; i++) {
-    int idx = univ[i].second;
-
-    if (check[idx] == 0) {
-      sum += univ[i].first;
-      check[idx] = 1;
-    } else {
-      for (int j = idx - 1; j >= 1; j--) {
-        if (check[j] == 0) {
-          check[j] = 1;
-          sum += univ[i].first;
-          break;
-        }
-      }
-    }
+  for (int i = 1; i <= max_day; i++) {
+    sum += days[i];
   }
 
   cout << sum;
