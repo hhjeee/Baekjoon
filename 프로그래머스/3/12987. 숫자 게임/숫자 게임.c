769c5ee37c0eda25339f7e8ab0d006c775ec1894
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-04-20 02:04:35 +0900

    [level 3] Title: 숫자 게임, Time: 10.60 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// A_len은 배열 A의 길이입니다.
+// B_len은 배열 B의 길이입니다.
+int solution(int A[], size_t A_len, int B[], size_t B_len) {
+    int answer = 0;
+    return answer;
+}
