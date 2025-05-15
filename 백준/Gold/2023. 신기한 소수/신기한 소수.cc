5b8f30ae488c6c76b9bd9cc05fa4c4d4c66af6c3
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-05-15 13:11:01 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+vector<int> result;
+
+bool checking_ok() {
+  string tmp = "";
+  for (int i = 0; i < result.size(); i++) {
+    tmp += to_string(result[i]);
+  }
+
+  int val = stoi(tmp);
+  if (val == 1)
+    return false;
+  for (int i = 2; i <= sqrt(val); i++) {
+    if (val % i == 0)
+      return false;
+  }
+
+  return true;
+}
+
+void dfs() {
+  if (result.size() == n) {
+    if (checking_ok()) {
+      for (int r : result) {
+        cout << r;
+      }
+      cout << '\n';
+    }
+    return;
+  }
+
+  for (int i = 1; i <= 9; i++) {
+    result.push_back(i);
+
+    if (checking_ok())
+      dfs();
+
+    result.pop_back();
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  cin >> n;
+
+  dfs();
+
+  return 0;
+}
